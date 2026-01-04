01faa8a686907d73d8fd1ba0526384f6a5327dea
DDevaras <59032984+DDevaras@users.noreply.github.com>
2026-01-03 20:23:18 -0800

    JPGtoPNGConverter.py

--- JPGtoPNGConverter.py
+++ JPGtoPNGConverter.py
@@ -3,22 +3,21 @@ import os
 from PIL from import Image
 
 
-#grab first and second argument which is Pokedex/ & new/
 images_folder = sys.argv[1]
-output_folder = sys.argv[2]
+new_folder = sys.argv[2]
 
 
 #check if new/ exists, if not create it
-if not os.path.exists(output_folder):
-  os.makedirs(output_folder)
+if not os.path.exists(new_folder):
+  os.makedirs(new_folder)
 
 
 for filename in os.listdir(images_folder):
   img = Image.open(f'{images_folder}{filename}')
   clean_name = os.path.splitext(filename)
   #Below says save as png and .png file format
-  img.save(f'{output_folder}{clean_name}.png', 'png')
+  img.save(f'{new_folder}{clean_name}.png', 'png')
   print('all done!')
-#Loop through Pokedex
+#Loop through
 #Convert images to PNG
-#Save to new folder
+#Saves to a new folder
